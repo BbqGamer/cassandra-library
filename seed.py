8a330e1ca38214ca38f4a170bea4d67f97ff2b8d
BbqGamer <korba.adam@gmail.com>
2024-06-04 11:10:08 +0200

    Refactor stress tests

--- seed.py
+++ seed.py
@@ -4,65 +4,70 @@ import uuid
 
 from cassandra.cluster import Cluster
 
-BOOKS_FILE = "data/books.csv"
-if not os.path.exists(BOOKS_FILE):
-    print("Books dataset should be in path: ", BOOKS_FILE)
+
+def seed():
+    BOOKS_FILE = "data/books.csv"
+    if not os.path.exists(BOOKS_FILE):
+        print("Books dataset should be in path: ", BOOKS_FILE)
+
+    cluster = Cluster([("127.0.0.1", 9042), ("127.0.0.1", 9043), ("127.0.0.1", 9044)])
+    session = cluster.connect()
+
+    print("Cleaning keyspace")
+    session.execute("DROP KEYSPACE IF EXISTS library")
+
+    keyspace_query = """
+    CREATE KEYSPACE IF NOT EXISTS library
+    WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}
+    """
+    session.execute(keyspace_query)
+    print("Created keyspace")
+
+    books_query = """
+    CREATE TABLE IF NOT EXISTS library.books (
+        id UUID PRIMARY KEY,
+        title TEXT,
+        author TEXT
+    );
+    """
+
+    session.execute(books_query)
+    print("Created books table")
+
+    reservations_query = """
+    CREATE TABLE IF NOT EXISTS library.reservations (
+        id UUID PRIMARY KEY,
+        book_id UUID,
+        email TEXT
+    );
+    """
+
+    session.execute(reservations_query)
+    print("Created reservations table")
+
+    reservations_by_book_query = """
+    CREATE TABLE IF NOT EXISTS library.reservations_by_book (
+        book_id UUID,
+        id UUID,
+        email TEXT,
+        PRIMARY KEY (book_id, id)
+    );
+    """
+
+    session.execute(reservations_by_book_query)
+    print("Created reservations by book table")
+
+    print("[SEEDING]")
+    with open(BOOKS_FILE, "r") as f:
+        cr = csv.reader(f)
+        _ = next(cr)
+        for row in cr:
+            book_id = uuid.uuid4()
+            session.execute(
+                """INSERT INTO library.books (id, title, author) VALUES (%s, %s, %s)""",
+                (book_id, row[0], row[1]),
+            )
 
 
-cluster = Cluster([("127.0.0.1", 9042), ("127.0.0.1", 9043), ("127.0.0.1", 9044)])
-session = cluster.connect()
-
-print("Cleaning keyspace")
-session.execute("DROP KEYSPACE IF EXISTS library")
-
-keyspace_query = """
-CREATE KEYSPACE IF NOT EXISTS library
-WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}
-"""
-session.execute(keyspace_query)
-print("Created keyspace")
-
-books_query = """
-CREATE TABLE IF NOT EXISTS library.books (
-    id UUID PRIMARY KEY,
-    title TEXT,
-    author TEXT
-);
-"""
-
-session.execute(books_query)
-print("Created books table")
-
-reservations_query = """
-CREATE TABLE IF NOT EXISTS library.reservations (
-    id UUID PRIMARY KEY,
-    book_id UUID,
-    email TEXT
-);
-"""
-
-session.execute(reservations_query)
-print("Created reservations table")
-
-reservations_by_book_query = """
-CREATE TABLE IF NOT EXISTS library.reservations_by_book (
-    book_id UUID,
-    id UUID,
-    email TEXT,
-    PRIMARY KEY (book_id, id)
-);
-"""
-
-session.execute(reservations_by_book_query)
-print("Created reservations by book table")
-
-print("[SEEDING]")
-with open(BOOKS_FILE, "r") as f:
-    cr = csv.reader(f)
-    header = next(cr)
-    for row in cr:
-        book_id = uuid.uuid4()
-        session.execute(
-            """INSERT INTO library.books (id, title, author) VALUES (%s, %s, %s)""",
-            (book_id, row[0], row[1]),
-        )
+if __name__ == "__main__":
+    seed()
--- stress_tests.py
+++ stress_tests.py
@@ -2,41 +2,52 @@ import random
 import time
 
 from cassandra_db import CassandraDB
-from main import confirm_reservation
+from main import confirm_reservation, display_reservations
+from seed import seed
 
 
-def stress_test_1():
+def stress_1(db):
     """The client makes the same request very quickly (10000 times)."""
-    db = CassandraDB()
-
     book_id = db.select_all_books()[0].id
-
-    print("Running stress test 1")
-    start = time.time()
     for _ in range(10000):
-        confirm_reservation(db, [book_id], "tester@gmail.com", True)
-    print(f"Finished stress test 1 in: {time.time() - start:.2f} seconds")
+        confirm_reservation(db, [book_id], "tester@gmail.com", auto=True)
 
 
-def stress_test_2():
+def stress_2(db):
     """Two or more clients make the possible requests randomly (10000 times)."""
-    db = CassandraDB()
-
     books = db.select_all_books()
 
     def random_request(user_email, books):
         num_chosen = random.randint(0, len(books) - 1)
         books_ids = [book.id for book in random.sample(books, num_chosen)]
-        confirm_reservation(db, books_ids, user_email, True)
+        confirm_reservation(db, books_ids, user_email, auto=True)
 
-    print("Running stress test 2")
-    start = time.time()
     for _ in range(10000 // 2):
         for user_email in ["first@test.com", "second@test.com"]:
             random_request(user_email, books)
-    print(f"Finished stress test 2 in: {time.time()-start:.2f} seconds")
+
+
+def stress_3(db):
+    """Immediate occupancy of all seats/reservations by 2 clients.
+    Idea is we have one pool for reservation and 2 clients want to claim as much as possible.
+    A situation where one client claims all Is undesirable"""
+    all_book_ids = [book.id for book in db.select_all_books()]
+    confirm_reservation(db, all_book_ids, "foo", auto=True)
+    confirm_reservation(db, all_book_ids, "bar", auto=True)
+
+
+def run_test(func):
+    seed()
+    db = CassandraDB()
+    start = time.time()
+
+    print(f"\nStarting {func.__name__}")
+    func(db)
+    print(f"Finished running {func.__name__} in: {time.time()-start:.2f} seconds")
+    display_reservations(db)
 
 
 if __name__ == "__main__":
-    stress_test_1()
-    stress_test_2()
+    run_test(stress_1)
+    run_test(stress_2)
+    run_test(stress_3)
