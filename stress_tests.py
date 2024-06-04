4a746785b1dec8c7a8d21171b225993e6bec04cf
BbqGamer <korba.adam@gmail.com>
2024-06-04 02:25:59 +0200

    Add stress test 2

--- stress_tests.py
+++ stress_tests.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from cassandra_db import CassandraDB
@@ -14,9 +15,28 @@ def stress_test_1():
     start = time.time()
     for _ in range(10000):
         confirm_reservation(db, [book_id], "tester@gmail.com", True)
-    elapsed = time.time() - start
-    print(f"Finished stress test 1 in: {elapsed:.2f} seconds")
+    print(f"Finished stress test 1 in: {time.time() - start:.2f} seconds")
+
+
+def stress_test_2():
+    """Two or more clients make the possible requests randomly (10000 times)."""
+    db = CassandraDB()
+
+    books = db.select_all_books()
+
+    def random_request(user_email, books):
+        num_chosen = random.randint(0, len(books) - 1)
+        books_ids = [book.id for book in random.sample(books, num_chosen)]
+        confirm_reservation(db, books_ids, user_email, True)
+
+    print("Running stress test 2")
+    start = time.time()
+    for _ in range(10000 // 2):
+        for user_email in ["first@test.com", "second@test.com"]:
+            random_request(user_email, books)
+    print(f"Finished stress test 2 in: {time.time()-start:.2f} seconds")
 
 
 if __name__ == "__main__":
     stress_test_1()
+    stress_test_2()
